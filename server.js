d67dd9e26c7d5ab325183cb6900e03273ad22f30
Amanda Gault <amandagault@dhcp-10-102-249-106.wireless.northwestern.private>
2017-09-23 08:53:12 -0500

    set up server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var logger = require("morgan")
+var mongoose = require("mongoose")
+
+mongoose.Promise = Promise
+
+var port = process.env.PORT || 3000;
+
+var app = express()
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(express.static("public"))
+
+var local_db = "mongodb://localhost/newsscrape"
+
+if(process.env.MONGODB_URI){
+    mongoose.connect(process.env.MONGODB_URI, function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log("Mongoose connection successful.");
+        }
+    });
+}
+else{
+    mongoose.connect(local_db, function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log("Mongoose connection successful.");
+        }
+    });
+}
+// Hook mongoose connection to db
+var db = mongoose.connection;
+
+// Log any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+require("./routes/routing")(app, db);
+
+// Listen on port 3000
+app.listen(port, function() {
+    console.log(`Listening on PORT ${port}`);
+});
